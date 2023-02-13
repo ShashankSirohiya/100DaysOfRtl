9bb66f041fa3b2b0770af68098cfdfaf8cd90735
ShashankSirohiya <sirohiya1625@gmail.com>
2023-02-13 10:47:22 +0530

    Add four files
    100DaysOfRtl

---
+++ Day1_ALU/ALU.v
@@ -0,0 +1,33 @@
+module ALU (output reg [8:0]result, 
+			input [7:0]d0,  // first input
+			input [7:0]d1, 	// second input
+			input [3:0]sel  // select
+	);
+always@(*)
+begin
+	case(sel)
+		4'b0000 : begin result = d0 + d1 ;  $display("0-Addition operation"); end
+		4'b0001 : begin result = d0 - d1 ;  $display("1-Subtraction operation"); end
+		4'b0010 : begin result = d0 * d1 ;  $display("2-Multiplication operation"); end  
+		4'b0011 : begin result = d0 / d1 ;  $display("3-Division operation"); end
+
+		4'b0100 : begin result = d0 && d1 ; $display("4-Logical AND operation"); end
+		4'b0101 : begin result = d0 ||d1 ;  $display("5-Logical OR operation"); end
+		4'b0110 : begin	result = ~(d0&d1) ; $display("6-Not of AND operation"); end 
+		4'b0111 : begin result = ~(d0|d1) ; $display("7-Not of OR operation"); end  
+	
+		4'b1000 : begin result = d0<<1 ;    $display("8-Left Shift operation"); end
+		4'b1001 : begin result = d1<<1 ; 	$display("9-Left Shift operation"); end
+		4'b1010 : begin result = d0>>1 ; 	$display("10-Right Shift operation"); end  
+		4'b1011 : begin result = d1>>1 ; 	$display("11-Right Shift operation"); end 
+		
+		4'b1100 : begin result = !d0 ; 		$display("12-Logical NOT operation"); end
+		4'b1101 : begin result = d0>>>1 ; 	$display("13-ArithmeticRightShiftoperation"); end
+		4'b1110 : begin result = ~d0 + 8'b00000001 ; $display("14-2's Compliment operation"); end 
+		4'b1111 : begin result = d0^d1 ; 	$display("15-XOR operation"); end
+		default : begin result = 8'bxxxxxxxx ; 		 $display("Default"); end
+	endcase // sel
+end
+endmodule : ALU
+
+
---
+++ Day1_ALU/tb_ALU.v
@@ -0,0 +1,103 @@
+module tb_ALU;
+	reg [7:0]d0;
+	reg [7:0]d1;
+	reg [3:0]sel;
+	wire [8:0]result;
+
+ ALU dut (
+ 		.result(result),
+		.d0(d0),			
+		.d1(d1),
+		.sel(sel)
+		);
+
+	initial
+	begin
+		d0[7:0]=0;
+		d1[7:0]=0;
+		sel[3:0]=0;
+	#5;
+		d0[7:0]=8'b00000011;
+		d1[7:0]=8'b00000001;
+		sel[3:0]=4'b0000;   	//Addition operation
+
+	#5;
+		d0[7:0]=8'b00000011;
+		d1[7:0]=8'b00000001;
+		sel[3:0]=4'b0001;       //Subtraction operation
+
+	#5;
+		d0[7:0]=8'b00000011;
+		d1[7:0]=8'b00000011;
+		sel[3:0]=4'b0010; 		//Multiplication operation
+
+	#5;
+		d0[7:0]=8'b00001000;
+		d1[7:0]=8'b00000010;
+		sel[3:0]=4'b0011; 		//Division operation
+
+	#5;
+		d0[7:0]=8'b00000010;
+		d1[7:0]=8'b10100010;
+		sel[3:0]=4'b0100;  		//Logical AND operation ===========
+
+	#5;
+		d0[7:0]=8'b00000010;
+		d1[7:0]=8'b00000010;
+		sel[3:0]=4'b0101;	    //Logical OR operation ==================
+
+	#5;
+		d0[7:0]=8'b00000010;
+		d1[7:0]=8'b00000010;
+		sel[3:0]=4'b0110;		//Not of AND operation ==================
+
+	#5;
+		d0[7:0]=8'b00000010;
+		d1[7:0]=8'b00000010;
+		sel[3:0]=4'b0111;		//Not of OR operation  =============
+ 
+	#5;
+		d0[7:0]=8'b00000010;
+		//d1[7:0]=8'b00000010;
+		sel[3:0]=4'b1000;		//Left Shift operation =================
+
+	#5;
+		//d0[7:0]=8'b00000010;
+		d1[7:0]=8'b00000010;
+		sel[3:0]=4'b1001;		//Left Shift operation
+
+	#5;
+		d0[7:0]=8'b00000100;
+		//d1[7:0]=8'b0;
+		sel[3:0]=4'b1010;  		//Right Shift operation
+
+	#5;
+		//d0[7:0]=8'b0;
+		d1[7:0]=8'b00000100;
+		sel[3:0]=4'b1011;		//Right Shift operation
+
+	#5;
+		d0[7:0]=8'b00000100;
+		//d1[7:0]=8'b0;
+		sel[3:0]=4'b1100;		//Logical NOT operatio
+	#5;
+		d0[7:0]=8'b00000010;
+		//d1[7:0]=8'b00000010;
+		sel[3:0]=4'b1101;		//ArithmeticRightShiftoperation
+
+	#5;
+		d0[7:0]=8'b00000010;
+		//d1[7:0]=8'b0;
+		sel[3:0]=4'b1110;		//2's compliment
+
+	#5;
+		d0[7:0]=8'b00000010;
+		d1[7:0]=8'b00000001;
+		sel[3:0]=4'b1111;  		//XOR operation
+
+end
+endmodule
+
+
+
+
