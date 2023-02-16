1d3b6cc461f5ccbbd6f2d35361a484ff62767a25
ShashankSirohiya <sirohiya1625@gmail.com>
2023-02-16 17:23:22 +0530

    fourth commit
    upload four files

---
+++ Day4_3_8Decoder/Decoder3_8.v
@@ -0,0 +1,18 @@
+module Decoder3_8(input [2:0] i,
+   				  output reg[7:0] y);
+	always@(i)
+		begin
+		y=0;
+			case(i)	
+					3'b000 : y[0] = 1'b1;
+					3'b001 : y[1] = 1'b1;
+					3'b010 : y[2] = 1'b1;
+					3'b011 : y[3] = 1'b1;
+					3'b100 : y[4] = 1'b1;
+					3'b101 : y[5] = 1'b1;
+					3'b110 : y[6] = 1'b1;
+					3'b111 : y[7] = 1'b1;
+			endcase
+		end	
+endmodule : Decoder3_8
+
---
+++ Day4_3_8Decoder/tb_Decoder3_8.v
@@ -0,0 +1,30 @@
+module tb_Decoder3_8 ;
+	reg [2:0] i;
+	wire [7:0] y;
+
+	Decoder3_8 uut (
+		.i(i), 
+		.y(y)
+	);
+	
+	initial
+	begin
+		i=3'b000;
+		#10;
+	end
+
+	always begin
+		#10 i=3'b000;
+		#10 i=3'b001;
+		#10 i=3'b010;
+		#10 i=3'b011;
+		#10 i=3'b100;
+		#10 i=3'b101;
+		#10 i=3'b110;
+		#10 i=3'b111;
+	end
+endmodule : tb_Decoder3_8
+
+
+
+
