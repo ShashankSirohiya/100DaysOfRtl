91f1dc5c77a833d63215b0e176103c81a25bb353
Shashank Sirohiya <77222438+ShashankSirohiya@users.noreply.github.com>
2023-02-22 10:34:15 +0530

    Update tb_Paramaterised_full_adder.v

--- Day10_Full_Adder/tb_Paramaterised_full_adder.v
+++ Day10_Full_Adder/tb_Paramaterised_full_adder.v
@@ -1,4 +1,5 @@
 module tb_Paramaterised_full_adder;
+	
 		defparam dut0.size=8;
 		reg [dut0.size-1:0]a;
 		reg [dut0.size-1:0]b;
@@ -6,36 +7,34 @@ module tb_Paramaterised_full_adder;
 		wire [dut0.size-1:0]sum;
 		wire carry;
 
-Paramaterised_full_adder dut0 (.a(a),
+                Paramaterised_full_adder dut0 (.a(a),
 				          .b(b),
 				          .cin(cin),
 				          .sum(sum),
-				          .carry(carry)
-  							  );
+					  .carry(carry) );
     
-						defparam dut1.size=16;
-						reg [dut1.size-1:0]a_16;
-						reg [dut1.size-1:0]b_16;
-						reg cin_16;
-						wire [dut1.size-1:0]sum_16;
-						wire carry_16;
+	
+		 defparam dut1.size=16;
+		 reg [dut1.size-1:0]a_16;
+		 reg [dut1.size-1:0]b_16;
+		 reg cin_16;
+		 wire [dut1.size-1:0]sum_16;
+		 wire carry_16;
 
-				Paramaterised_full_adder dut1 (.a(a_16),
-								          .b(b_16),
-								          .cin(cin_16),
-								          .sum(sum_16),
-								          .carry(carry_16)
-				   								 );
+	         Paramaterised_full_adder dut1 (.a(a_16),
+					        .b(b_16),
+					        .cin(cin_16),
+					        .sum(sum_16),
+					        .carry(carry_16) );
 				    
     
-			initial
-			begin
+			initial begin
 			    a=0;
 			    	a_16=0;
 			    b=0;
 			    	b_16=0;
 			    cin=0;
-			   		cin_16=0;
+			   	cin_16=0;
 			    #10;    
 			end
 
@@ -45,11 +44,7 @@ Paramaterised_full_adder dut0 (.a(a),
 			    b=$random;
 			    	b_16=$random;
 			    cin=$random;
-			   		cin_16=$random;
-			    
+			   	cin_16=$random;   
 			    #20;
 			end   
 endmodule : tb_Paramaterised_full_adder
-
-
-
