c080fd61e2beb502cd89d4b88e5ac06cbd2197aa
ShashankSirohiya <sirohiya1625@gmail.com>
2023-03-01 13:25:56 +0530

    seventeenth commit
    upload four files

---
+++ Day17_Demultiplexer/Demultiplexer.v
@@ -0,0 +1,14 @@
+module Dmux(input i,
+			input [1:0] s,
+			output reg [3:0] y);
+	always@(i,s)
+		begin
+		y=0;
+			case (s)
+				2'b00 : y[0] = i;
+				2'b01 : y[1] = i;
+				2'b10 : y[2] = i;
+				2'b11 : y[3] = i;
+			endcase
+		end
+endmodule : Dmux
---
+++ Day17_Demultiplexer/tb_Demultiplexer.v
@@ -0,0 +1,22 @@
+module tb_Dmux;
+		reg i;
+		reg [1:0] s;
+		wire [3:0] y;
+
+	Dmux uut (.i(i), 
+			  .s(s), 
+			  .y(y));
+		initial 
+		begin
+			i = 0;
+			s = 0;
+			#50;
+		end
+		
+		always
+		begin
+			#100;
+			i=$random;
+			s=$random;
+		end      
+endmodule : tb_Dmux
