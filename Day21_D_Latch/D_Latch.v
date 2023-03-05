b8c22c2a88442adc06274e18833eeb9274c584a3
Shashank Sirohiya <77222438+ShashankSirohiya@users.noreply.github.com>
2023-03-06 00:11:31 +0530

    Update D_Latch.v

--- Day21_D_Latch/D_Latch.v
+++ Day21_D_Latch/D_Latch.v
@@ -1,20 +1,16 @@
-module DLatch_NB(
-		input d,en,
-		output reg q,qbar
-    );
+module DLatch_NB(input d,en,
+		output reg q,qbar);
 
 	always@(d,en)
 		begin
 			if(~en) 
 			begin
-				q <= q;
-				qbar <= qbar;
+			     q <= q;
+			     qbar <= qbar;
 			end
-			else
-			begin
-				q <= d;
-				qbar <= (~d);
+			else begin
+			     q <= d;
+			     qbar <= (~d);
 			end
-		end
-		
+		end	
 endmodule
