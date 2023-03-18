926d0f1c70492b4a68c92a7dee1c424b8fa42434
ShashankSirohiya <sirohiya1625@gmail.com>
2023-03-18 21:21:39 +0530

    33rd commit
    upload four files

---
+++ Day33_FSM_Traffic_Light_Controller/FSM_Traffic_Light_Controller.v
@@ -0,0 +1,73 @@
+module tlc_thursday(
+	input clk,reset,pass,
+	output reg[1:0]state,
+	output reg red,yellow,green
+	);
+	localparam redsignal=2'b00,redwithyellow=2'b01,greensignal=2'b10,greenwithyellow=2'b11;
+
+	always@(posedge clk)
+		begin
+			if(reset==1'b1) begin
+					red<=1'b1;
+					yellow<=1'b0;
+					green<=1'b0;
+					state<=redsignal;
+				end
+
+				else if(pass==1'b1) begin
+				    red<=1'b0;
+					yellow<=1'b0;
+					green<=1'b1;
+					state<=greensignal;
+				end
+
+			else begin
+				case(state)
+					redsignal:
+						begin
+							red<=1'b1;
+							yellow<=1'b0;
+							green<=1'b0;
+							#10;
+							state<=redwithyellow;
+						end
+
+					redwithyellow:
+						begin
+							red<=1'b1;
+							yellow<=1'b1;
+							green<=1'b0;
+							#10;
+							state<=greensignal;
+						end
+
+					greensignal:
+						begin
+							red<=1'b0;
+							yellow<=1'b0;
+							green<=1'b1;
+							#10;
+							state<=greenwithyellow;
+						end
+
+					greenwithyellow:	
+						begin
+							red<=1'b0;
+							yellow<=1'b1;
+							green<=1'b1;
+							#10;
+							state<=redsignal;
+						end
+					
+					default:
+						begin
+						   red<=1'bz;
+						   yellow<=1'bz;
+						   green<=1'bz;
+						   #10;
+						   state<=redsignal;
+						end
+				endcase
+			end
+		end
+endmodule
---
+++ Day33_FSM_Traffic_Light_Controller/tb_FSM_Traffic_Light_Controller.v
@@ -0,0 +1,37 @@
+module tb_tlc_thursday;
+        reg tb_rst,tb_clk;
+        reg tb_pass;
+        wire tb_red;
+        wire tb_yellow;
+        wire tb_green;
+        wire [1:0]tb_state;
+
+        tlc_thursday uut0 (         
+                .reset(tb_rst),
+                .clk(tb_clk),
+                .pass(tb_pass),
+                .red(tb_red),
+                .yellow(tb_yellow),
+                .green(tb_green),
+                .state(tb_state)
+                        ); 
+
+                always #5 tb_clk = ~tb_clk;
+
+                initial 
+                begin
+                tb_clk=1;
+                tb_rst=1;
+                tb_pass=0;
+                #30;
+                tb_rst=0;
+                #5
+                tb_pass=1;
+                #10;
+                tb_pass=0;
+                #30;
+
+                #100 $finish;
+                end
+ 
+endmodule
